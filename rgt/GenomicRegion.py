766f6c9d2b1cec12c5ad0232ec6c020499160813
manuel.allhoff <manuel.allhoff@gmail.com>
2013-10-15 12:45:02 +0000

    add docstrings to class

--- rgt/GenomicRegion.py
+++ rgt/GenomicRegion.py
@@ -1,64 +1,82 @@
+"""
+Authors: Ivan G. Costa, Manuel Allhoff
+
+A GenomicRegion describes a genomic region.
+
+Methods:
+
+extend(left, right):
+Extend the region to the left and right side.
+
+overlap(region):
+Return true if region overlaps with argument-region, otherwise false.
+
+"""
+
 class GenomicRegion:
 
-  def __init__(self,chr,initial,final,name=None,orientation=None,data=None):
-    self.chr=chr
-    self.initial=initial
-    self.final=final
-    self.name=name
-    self.orientation=orientation
-    self.data=data
+    def __init__(self, chrom, initial, final, name=None, orientation=None, data=None):
+        """Initialize GenomicRegion"""
+        self.chrom = chrom
+        self.initial = initial
+        self.final = final
+        self.name = name
+        self.orientation = orientation
+        self.data = data
 
-  def __len__(self):
-    return self.final-self.initial
+    def __len__(self):
+        """Return length of GenomicRegion"""
+        return self.final - self.initial + 1
 
 
-  def __str__(self):
-    s= self.chr+'\t'+str(self.initial)+'\t'+str(self.final)
-    if self.name != None:
-      s=s+'\t'+self.name
-    if self.orientation != None:
-      s=s+'\t'+self.orientation
-    if self.data != None:
-      s=s+'\t'+self.data
-    return s
+    def __str__(self):
+        """Give informal string representation"""
+        s = '\t'.join( [self.chrom, str(self.initial), str(self.final)] )
+        if self.name is not None:
+            s += '\t' + self.name
+        if self.orientation is not None:
+            s += '\t' + self.orientation
+        if self.data is not None:
+            s += '\t' + self.data
+        return s
 
-  def extend(self,left,right):
-    self.initial-=left
-    self.final+=right    
+    def extend(self, left, right):
+        """Extend GenmocRegion both-sided"""
+        #TODO: What is about orientation?
+        self.initial -= left
+        self.final += right    
 
-  def overlapp(self,region):
-    if region.chr == self.chr:
-        if self.initial < region.initial:
-            if self.final >= region.initial:
-                return True
-        else:
-            if self.initial <= region.final:
-                return True
-    return False
+    def overlap(self, region):
+        """Return True, if GenomicRegion overlaps with region, else False."""
+        if region.chrom == self.chrom:
+            if self.initial < region.initial:
+                if self.final >= region.initial:
+                    return True
+                else:
+                    if self.initial <= region.final:
+                        return True
+        return False
 
                     
-  def __repr__(self):
-    return self.chr+','+str(self.initial)+','+str(self.final)
-  
+    def __repr__(self):
+        """Return official representation of GenomicRegion"""
+        return ','.join( [self.chrom, str(self.initial), str(self.final)] )
 
-  def __cmp__(x,y):
-    '''  The return value is negative if x < y, zero if x == y and strictly positive if x > y. '''
-    if x.chr<y.chr:
-      return -1
-    elif x.chr>y.chr:
-      return 1
-    else:
-      if x.initial < y.initial:
-        return -1
-      elif x.initial > y.final:
-        return 1
-      else:
-        if x.final < y.final:
-          return -1
-        elif x.final > y.final:
-          return 1
+    def __cmp__(self, x, y):
+        """Return negative value if x < y, zero if x == y and strictly positive if x > y"""
+        if x.chrom < y.chrom:
+            return -1
+        elif x.chrom > y.chrom:
+            return 1
         else:
-          return 0
-
-
-
+            if x.initial < y.initial:
+                return -1
+            elif x.initial > y.final:
+                return 1
+            else:
+                if x.final < y.final:
+                    return -1
+                elif x.final > y.final:
+                    return 1
+                else:
+                    return 0
