9dd1020b632c4132b603f26672ecfa3ed565de7f
jovesus@gmail.com <jovesus@gmail.com@7ac398c0-e0aa-3817-0650-6b828aca5894>
2014-01-29 15:53:23 +0000

    Add some comments to ExperimentalMatrix

--- trunk/rgt/ExperimentalMatrix.py
+++ trunk/rgt/ExperimentalMatrix.py
@@ -1,17 +1,20 @@
-possible_types=["genes","regions","reads"]
+from __future__ import print_function
 from rgt.GenomicRegionSet import *
 from rgt.GeneSet import *
 import numpy
 
+
+possible_types=["genes","regions","reads"]
+
 class ExperimentalMatrix:
 
     def __init__(self):
-        self.fields=[]
-        self.fieldsDict={}
-        self.names=[]
-        self.files={}
-        self.types=[]
-        self.objectsDict={}
+        self.names=[] # the name of experiment (filename)
+        self.types=[] # the type of data
+        self.files={} # the path of the related file with its filename
+        self.fields=[] # list types of informations including names, types, files and others
+        self.fieldsDict={} # its keys are just self.fields, and the values are extra informations        
+        self.objectsDict={} # key is the names; value is GenomicRegionSet or GeneSet
     
     def read(self,file):
         f=open(file)
@@ -36,20 +39,19 @@ class ExperimentalMatrix:
                     dict[line[fi]].append(line[0])
                 except:
                     dict[line[fi]]=[line[0]]
-	self.types=numpy.array(self.types)
-	self.names=numpy.array(self.names)
+        self.types=numpy.array(self.types)
+        self.names=numpy.array(self.names)
         self.load_objects()
 
     def get_genesets(self):
-	return [self.objectsDict[i] for i in self.names[self.types=="genes"]]
+	    return [self.objectsDict[i] for i in self.names[self.types=="genes"]]
 
     def get_regionsets(self):
         return [self.objectsDict[i] for i in self.names[self.types=="regions"]]
 
-
     def load_objects(self):
         for i,t in enumerate(self.types):
-            print self.files[self.names[i]]
+            print(self.files[self.names[i]])
             if t == "regions":
                 bed = GenomicRegionSet(self.names[i])
                 bed.read_bed(self.files[self.names[i]])
@@ -58,5 +60,3 @@ class ExperimentalMatrix:
                 genes= GeneSet(self.names[i])
                 genes.read(self.files[self.names[i]])
                 self.objectsDict[self.names[i]]=genes
-
-            
