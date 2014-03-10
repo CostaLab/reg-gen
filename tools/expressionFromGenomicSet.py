124bf0d5082167d312b5bcb25d062e20db17d6e0
manuel.allhoff <manuel.allhoff@gmail.com>
2014-03-10 12:31:05 +0000

    add some comments and help

--- tools/expressionFromGenomicSet.py
+++ tools/expressionFromGenomicSet.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
+%prog <experimental matrix file> <gene expression file> <annotation path> <output dir>
+
 A intersectGenomicSets perform intersection statistics for a set of bed files.
 
 Authors: Ivan G. Costa, Manuel Allhoff
@@ -7,58 +11,79 @@ It recieves as input a experimental matrix with a list of bed files and outputs 
 
 """
 
-
+from __future__ import print_function
+from optparse import OptionParser
 import sys
 from rgt.ExperimentalMatrix import *
 from rgt.GenomicRegionSet import *
 from rgt.CoverageSet import *
 import numpy
 
-def averageExpression(bed,expression,regionsToGenes):
-    values=numpy.zeros((len(bed),len(expression.cond)))
-    labels=[]
-    for i,b in enumerate(bed):
-        region=b.toString()
-        labels.append(region)
-        try:
-          genes=regionsToGenes[region]
-          #print genes
-          for g in genes:
-            values[i,]+=expression.values[g.upper()]
-            #print expression.values[g.upper()]
-          values[i,]=values[i,]/float(len(genes))
-        except:
-          values[i,]='NaN'
-    return values,labels
 
-def printTable(namesCol,namesLines,table,fileName):
-    f=open(fileName,"w")
+def averageExpression(region, expression, regionsToGenes):
+    """Compute average gene expression"""
+    values = numpy.zeros((len(region), len(expression.cond)))
+    labels = []
+    for i, b in enumerate(region):
+        region_string = b.toString()
+        labels.append(region_string)
+        try:
+            genes = regionsToGenes[region_string]
+
+            for g in genes:
+                values[i,] += expression.values[g.upper()]
+
+            values[i,] = values[i,]/float(len(genes))
+        except:
+            values[i,] = 'NaN'
+    
+    return values, labels
+
+def output(namesCol, namesLines, table, file_name):
+    """Output table"""
+    f = open(file_name,"w")
     f.write("\t"+("\t".join(namesCol))+"\n")
     for i,line in enumerate(table):
-      f.write(namesLines[i]+"\t"+("\t".join([str(j) for j in line]))+"\n")
+        f.write(namesLines[i]+"\t"+("\t".join([str(j) for j in line]))+"\n")
 
+class HelpfulOptionParser(OptionParser):
+    """An OptionParser that prints full help on errors."""
+    def error(self, msg):
+        self.print_help(sys.stderr)
+        self.exit(2, "\n%s: error: %s\n" % (self.get_prog_name(), msg))
 
-out=""
-experimentalFile = sys.argv[1]
-exps=ExperimentalMatrix()
-exps.read(experimentalFile)
-beds = exps.get_regionsets()
-geneExp= sys.argv[2]
-outputFile = sys.argv[3]
-anotationPath = sys.argv[4]
-genomeFile=anotationPath+"chrom.sizes"
-geneFile=anotationPath+"association_file.bed"
-
-
-genes= GeneSet("Expression")
-genes.readExpression(geneExp)
-
-for bed in beds:
-  bedNew = GenomicRegionSet("")
-  [degenes,de_peak_genes, mappedGenes, totalPeaks,regionsToGenes] = bedNew.filter_by_gene_association(bed.fileName,genes,geneFile,genomeFile,threshDist=5000)
-  [ct,labels]=averageExpression(bed,genes,regionsToGenes)
-  aux=bed.fileName.split("/")
-  fileName=aux[-1]
-  printTable(genes.cond,labels,ct,outputFile+"/"+fileName[:-4]+".txt")
-
+if __name__ == '__main__':
+    parser = HelpfulOptionParser(usage=__doc__)
+    (options, args) = parser.parse_args()
+     
+    if len(args) != 4:
+            parser.error("Exactly three parameters are needed: experimental matrix, gene expression, annotation path and prefix for output")
+     
+    #map arguments
+    experimental_matrix_file = args[0]
+    gene_exp = args[1]
+    annotation_path = args[2]
+    outputdir = args[3]
+    
+    exps = ExperimentalMatrix()
+    exps.read(experimental_matrix_file)
+    regionsets = exps.get_regionsets()
+    
+    genome_file = annotation_path + "/chrom.sizes"
+    gene_file = annotation_path + "/association_file.bed"
+    
+    genes = GeneSet("Expression")
+    genes.readExpression(gene_exp)
+    
+    for region in regionsets:
+        bedNew = GenomicRegionSet("")
+        [degenes, de_peak_genes, mappedGenes, totalPeaks, regionsToGenes] \
+        = bedNew.filter_by_gene_association(region.fileName, genes, gene_file, genome_file, threshDist=5000)
+        
+        [ct, labels] = averageExpression(region, genes, regionsToGenes)
+        aux = region.fileName.split("/")
+        fileName = aux[-1]
+        output(genes.cond, labels, ct, outputdir + "/" + fileName[:-4] + ".txt")
+        
+        
 
