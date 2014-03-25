a2351f73d77613a9d40370bd0069381057a4303f
jovesus@gmail.com <jovesus@gmail.com@7ac398c0-e0aa-3817-0650-6b828aca5894>
2014-03-25 15:42:47 +0000

    Add more functions of plotTools

--- trunk/tools/plotTool.py
+++ trunk/tools/plotTool.py
@@ -2,25 +2,27 @@
 from __future__ import print_function
 from __future__ import division
 import sys
-import os.path
+import os
 lib_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(lib_path)
 import numpy
 from scipy.stats import mstats
 import matplotlib.pyplot as plt
+import time
 
 # Local Libraries
 # Distal Libraries
+from rgt.GenomicRegion import *
 from rgt.GenomicRegionSet import *
 from rgt.ExperimentalMatrix import *
 from rgt.Util import *
 from rgt.CoverageSet import *
 
 # Local test
-tool_path = "/media/931ef578-eebe-4ee8-ac0b-0f4690f126ed/projects/reg-gen/trunk/tools/plotTools.py"
+tool_path = "/media/931ef578-eebe-4ee8-ac0b-0f4690f126ed/projects/reggen/tools/plotTools.py"
 input_path = "/media/931ef578-eebe-4ee8-ac0b-0f4690f126ed/projects/box_plot/input_EM.txt"
-
-sys.argv = ["plotTools","boxplot",input_path]
+dir = os.getcwd()
+#sys.argv = ["plotTools","boxplot",input_path]
 
 """
 Plot tool for generating analytic figures.
@@ -44,6 +46,12 @@ def bedCoverage(bed,reads):
 
 def printTable(namesCol,namesLines,table,fileName):
     """ Write a txt file from the given table. """
+    d = os.path.dirname(fileName)
+    try:
+        os.stat(d)
+    except:
+        os.mkdir(d)
+             
     f = open(fileName,"w")
     f.write("\t"+("\t".join(namesCol))+"\n")
     for i,line in enumerate(table):
@@ -54,34 +62,15 @@ def printTable(namesCol,namesLines,table,fileName):
 def quantile_normalization(matrix):
     """ Return the np.array which contains the normalized values
     """
-    """
-    f = open(fileName)
-    data = []
-    for i, l in enumerate(f.readlines()):
-        if i == 0: 
-            list_BAM = l.split()
-            print("\tQuantile normalizing following datasets:")
-            for b in list_BAM:
-                print("\t\t",b)   
-        else:
-            data.append([int(j) for j in l.split()[1:]])
-    f.close()
-    #print(data)
-    matrix = numpy.array(data)
-    """
-    #print(matrix)
-    #matrix.astype(numpy.float)
+    
     rank_matrix = []
     for c in range(matrix.shape[1]):
         col = matrix[:,c]
-        #new_col = mstats.rankdata(numpy.ma.masked_invalid(col))
         rank_col = mstats.rankdata(col)
         rank_matrix.append(rank_col)
 
     ranks = numpy.array(rank_matrix)
     trans_rank = numpy.transpose(ranks)
-    #print(trans_rank)
-    #numpy.savetxt("rankfile.txt",ranks)
     
     # Calculate for means of ranks
     sort_matrix = numpy.sort(matrix,axis=0)
@@ -89,8 +78,6 @@ def quantile_normalization(matrix):
     for r in range(matrix.shape[0]):
         row = [x for x in sort_matrix[r,:]]
         means.append(numpy.mean(row))
-    #print(sort_matrix)
-    #print(means)
 
     # Replace the value by new means
     normalized_table = numpy.around(trans_rank)
@@ -102,27 +89,83 @@ def quantile_normalization(matrix):
 def tables_for_plot(norm_table,all_bed,beds):
     """ Return a Dict which stores all tables for each bed with bedname as its key. """
     tableDict = {} # Storage all tables for each bed with bedname as the key
-    for bedname in beds.keys():
-        tableDict[bedname] = []
-        for i,r in enumerate(all_bed.sequences):
-            if r in beds[bedname].sequences:
-                tableDict[bedname].append(norm_table[i])
+    conList = []   # Store containers of beds
+    iterList = []
+    
+    for i,bed in enumerate(beds):
+        tableDict[bed.name] = []
+        bed.sort()
+        conList.append(bed.__iter__())
+        iterList.append(conList[-1].next())
+        
+    for i, r in enumerate(all_bed.sequences):
+        for j in range(len(beds)):
+            while r > iterList[j]:
+                try:
+                    iterList[j] = conList[j].next()
+                except:
+                    break
+            if r == iterList[j]:
+                tableDict[beds[j].name].append(norm_table[i])
+            elif r < iterList[j]:
+                continue
     return tableDict
     
-def box_plot(tables,groupby = "bed"):
+def box_plot(tables,bednames,readnames,groupby = "bed"):
     """ Return boxplot from the given tables.
     
     Parameters:
         tables - a dictionary with bedname as key and normalized value as its value
-        groupby - Two ways to plot: 1. bed grouped; 2. bam grouped
-        
+        groupby - "bed" --> bed grouped; 
+                  "bam" --> bam grouped
     
     """
-    plt.Figure()
-    plt.boxplot(tables, notch=False, sym='o', vert=True, whis=1.5,
-                positions=None, widths=None, patch_artist=False,
-                bootstrap=None)
+    ## Prepare the data
+    
+    plotDict = {}
+    for bedname in tables.keys():
+        plotDict[bedname] = {}
+        mt = numpy.array(tables[bedname])
+        for i,readname in enumerate(readnames):
+            plotDict[bedname][readname] = mt[:,i]
+            
+    data = []
+    x_label = []
+    x_tick = []
+    nb = 8
+    if groupby == "bed":
+        for bed in plotDict.keys():
+            x_label.append(bed+" "*nb)
+            for bam in plotDict[bed].keys():
+                data.append(plotDict[bed][bam])
+                x_tick.append(bam)
+    elif groupby == "bam":
+        for bam in readnames:
+            x_label.append(bam+" "*nb)
+            for bed in plotDict.keys():
+                data.append(plotDict[bed][bam])
+                x_tick.append(bed)
+    
+    ## Initialize the figure
+    fig = plt.figure()
+    fig.subplots_adjust(bottom=0.3)
+    
+    # Plotting
+    bp = plt.boxplot(data, notch=False, sym='o', vert=True, whis=1.5,
+                     positions=None, widths=None, patch_artist=False, bootstrap=None)
+    
+    # Editting for details
+    plt.title("Boxplot of Gene Association analysis")
+    plt.ylabel("Count")
+    plt.savefig("boxplot.png") 
+    plt.setp(bp['boxes'], color='black')
+    plt.setp(bp['whiskers'], color='black')
+    plt.setp(bp['fliers'], markerfacecolor='none',color='gray',alpha=0.8)
+    plt.setp(bp['medians'], color='black', linewidth=2)
+    plt.xticks(range(1,len(x_tick)+1), x_tick, rotation=90)
+    plt.savefig("boxplot.png")
     plt.show()
+    
 #################################################################################################
 ##### PARAMETERS ################################################################################
 #################################################################################################
@@ -155,13 +198,14 @@ elif(len(sys.argv) == 2) and sys.argv[1] == "boxplot":
 ##### INPUT #####################################################################################
 #################################################################################################
 
+t0 = time.time()
 # Input parameters dictionary
 
 # Read the Experimental Matrix
 exps = ExperimentalMatrix()
-exps.read(input_path)
-beds = exps.get_regionsets()
-bednames = beds.keys()
+exps.read(sys.argv[2])
+beds = exps.get_regionsets() # A list of GenomicRegionSets
+bednames = exps.get_regionsnames()
 reads = exps.get_readsfiles()
 readsnames = exps.get_readsnames()
 
@@ -169,18 +213,42 @@ if sys.argv[2] and sys.argv[1] == "boxplot":
 
     # Combine the beds
     all_bed = GenomicRegionSet("All regions")
-    for bed in beds.values():
+    for bed in beds:
         all_bed.combine(bed)
-    all_bed.remove_duplicates()
+    all_bed.remove_duplicates() #all_bed is sorted!!
+    
+    t1 = time.time()
+    print("Step 1/5 finished: Combining " + str(len(all_bed.sequences)) + " regions from all bed files")
+    print("    --- required time: "+ str(t1-t0)+ " secs")
     
     # Coverage of reads on all_bed
     all_table = bedCoverage(all_bed,reads) 
-    printTable(readsnames,bednames,all_table,"cov/all_bed.txt")
+    regionnames = [r.chrom+":"+str(r.initial)+"-"+str(r.final) for r in all_bed]
+    printTable(readsnames,regionnames,all_table,os.path.join(dir,"cov/all_bed.txt"))
+    print("Step 2/5 finished: Calculating coverage of each bam file on all regions (saved as 'cov/all_bed.txt')")
+    t2 = time.time()
+    print("    --- required time: "+ str(t2-t1)+ " secs")
+    
     # Quantile normalization
     norm_table = quantile_normalization(all_table)
-    printTable(readsnames,bednames,norm_table,"cov/norm_bed.txt")
+    printTable(readsnames,regionnames,norm_table,os.path.join(dir,"cov/norm_bed.txt"))
+    print("Step 3/5 finished: Quantile normalization of all coverage table (saved as 'cov/norm_bed.txt')")
+    t3 = time.time()
+    print("    --- required time: "+ str(t3-t2)+ " secs")
     
     # Generate individual table for each bed
     tables = tables_for_plot(norm_table,all_bed, beds)
+    for bed in beds:
+        print(len(bed.sequences))
+        regionnames = [r.chrom+":"+str(r.initial)+"-"+str(r.final) for r in bed]
+        printTable(readsnames,regionnames,tables[bed.name],dir+"/cov/plot/"+bed.name+".txt")
+    print("Step 4/5 finished: Constructing different tables for box plot (saved in 'cov/plot/')")
+    t4 = time.time()
+    print("    --- required time: "+ str(t4-t3)+ " secs")
     
-    #box_plot(tables)
+    # Plotting
+    box_plot(tables,bednames,readsnames,groupby = "bed")
+    print("Step 5/5 finished:")
+    t5 = time.time()
+    print("    --- required time: "+ str(t5-t4)+ " secs")
+    
