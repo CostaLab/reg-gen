08dabe2a18bf9122b5831a617c23abfa044be41e
eggduzao <eggduzao@7ac398c0-e0aa-3817-0650-6b828aca5894>
2014-07-11 14:45:49 +0000

    Changes in GenomicRegionSet in order to add gene associations

--- rgt/GenomicRegionSet.py
+++ rgt/GenomicRegionSet.py
@@ -2,12 +2,12 @@ from __future__ import print_function
 from __future__ import division
 from GenomicRegion import *
 import random
-import copy
+from copy import deepcopy
 import os
 import time
 from scipy import stats
-from Util import GenomeData
-from Util import OverlapType
+from Util import GenomeData, OverlapType, AuxiliaryFunctions
+from GeneSet import GeneSet
 
 """
 Represent list of GenomicRegions.
@@ -123,29 +123,242 @@ class GenomicRegionSet:
             for s in self:
                 print(s, file=f)
 
-    def filter_by_gene_association(self,fileName,geneSet,geneAnnotation,genomeSize,promoterLength=1000,threshDist=50000):
-        """Associates genomic regions to genes given the following rules:
-             1. If the peak is inside gene (promoter+coding) then this peak is associated with that gene.
-             2. If a peak is inside overlapping genes, then the peak is annotated with both genes.
-             3. If peak is between two genes (not overlapping neither), then both genes are annotated.
-             4. If the distance between peak and gene is greater than a threshold distance, then it is not annotated.
+    def gene_association(self, gene_set = None, organism = "hg19", promoterLength = 1000, threshDist = 50000):
+        """Associates coordinates to genes given the following rules:
+           1. If the peak is inside gene (promoter+coding) then this peak is associated with that gene.
+           2. If a peak is inside overlapping genes, then the peak is annotated with both genes.
+           3. If peak is between two genes (not overlapping neither), then both genes are annotated.
+           4. If the distance between peak and gene is greater than a threshold distance, then it is not annotated.
+
+        Keyword arguments:
+        gene_set -- List of gene names as a GeneSet object. If None, then consider all genes to be enriched. (default None)
+        organism -- Organism in order to fetch genomic data. (default hg19)
+        promoterLength -- Length of the promoter region. (default 1000)
+        threshDist -- Threshold maximum distance for a coordinate to be considered associated with a gene. (default 50000)
+
+        Returns:
+        result_grs -- GenomicRegionSet exactely as self, but with the following additional information:
+                      1. name: String of genes associated with that coordinate separated by ':'
+                      2. data: String of proximity information (if the coordinate matched to the corresponding gene in the
+                               previous list in a proximal position (PROX) or distal position (DIST)) separated by ':'
+                      The gene will contain a '.' in the beginning of its name if it is not in the gene_set given.
+        """
+ 
+        # Initializations
+        chromosome_list = list(set(self.get_chrom()))
+        genome_data = GenomeData(organism)
+
+        # Separating coordinates by chromosomes
+        coord_dict = dict([(chrom,[]) for chrom in chromosome_list])
+        for gr in self.sequences:
+            coord_dict[gr.chrom].append([gr.initial,gr.final,gr.name,gr.data,gr.orientation])
+
+        # Sorting coord_dict
+        for chrom in coord_dict.keys(): coord_dict[chrom] = sorted(coord_dict[chrom], key=lambda x: x[0])
+
+        # Reading assocDict
+        gene_regions = GenomicRegionSet("gene_regions")
+        gene_regions.read_bed(genome_data.get_association_file())
+        assocDict = dict()
+        geneFlagDict = dict()
+        for gr in gene_regions:
+            geneFlagDict[gr.name.upper()] = False
+            curr_region = [gr.initial,gr.final,gr.name,0,gr.orientation]
+            try: assocDict[gr.chrom].append(curr_region)
+            except Exception: assocDict[gr.chrom] = [curr_region]
+       
+        # Sorting assocDict
+        for chrom in assocDict.keys(): assocDict[chrom] = sorted(assocDict[chrom], key=lambda x: x[0])
+        
+        # Updating assocDict to contain all chromosomes in coord_dict
+        for chrom in chromosome_list:
+            if(chrom not in assocDict.keys()): assocDict[k] = []
+
+        # Updating geneFlagDict based on gene_set
+        if(gene_set):
+            gene_list = [g.upper() for g in gene_set.genes]
+            for e in gene_list: geneFlagDict[e] = True
+        else: 
+            for e in geneFlagDict.keys(): geneFlagDict[e] = True
+
+        # Associating coord_dict with assocDict
+        aDict = dict() # Results dictionary
+        for chrName in chromosome_list:
+
+            aDict[chrName] = [] # Adding new result list
+            counter = 0 # Counter to run through gene list
+
+            # Iterating on coordinate list (main list)
+            for coord in coord_dict[chrName]: 
+
+                didBreak = False # Check wether it breaked or not.
+
+                # Running linearly through gene list
+                while(counter < len(assocDict[chrName])):
+
+                    # Extend the gene coordinates to encompass promoter region based on strandness
+                    if(assocDict[chrName][counter][4] == "+"): geneCoord = [assocDict[chrName][counter][0]-promoterLength,assocDict[chrName][counter][1]]
+                    else: geneCoord = [assocDict[chrName][counter][0],assocDict[chrName][counter][1]+promoterLength]
+
+                    check = AuxiliaryFunctions.overlap(coord,geneCoord) # Check overlap between coordinate and gene+promoter
+
+                    if(check == 0): # If contain overlap, then check if the coordinate also contains overlap with the next gene
+
+                        # Verify if next gene+promoter also overlap
+                        genesList = [assocDict[chrName][counter][2]+"_PROX"] # List of overlapping genes
+                        if(counter < len(assocDict[chrName])-1): # If this is not the last gene (there is a 'next') then verify overlap
+                            if(assocDict[chrName][counter+1][4] == "+"): geneCoordNext = [assocDict[chrName][counter+1][0]-promoterLength,assocDict[chrName][counter+1][1]]
+                            else: geneCoordNext = [assocDict[chrName][counter+1][0],assocDict[chrName][counter+1][1]+promoterLength]
+                            checkNext = AuxiliaryFunctions.overlap(coord,geneCoordNext) # Verify overlap between coordinate and next gene
+                            if(checkNext == 0): genesList.append(assocDict[chrName][counter+1][2]+"_PROX") # If there is an overlap then add this gene to association list
+                        #else: # If this is the last gene (there is no 'next') then dont verify overlap
+
+                        # Verify if genes are enriched
+                        for i in range(0,len(genesList)): # Check if genes in genesList are enriched 
+                            if(not geneFlagDict[genesList[i][:-5]]): genesList[i] = "."+genesList[i] # If the gene is not enriched, put a dot (.) in front of it (will be used by motif matching)
+                            #else: If gene is enriched than let the name without the dot (.) in front of it
+                        didBreak = True
+                        break
+
+                    elif(check == -1): # If gene is after coordinate, then check if they are within maximum distance for overlap. Also do the same check for the previous gene.
+
+                        # Verify overlap again using maximum distance with current gene+promoter
+                        genesList = [] # List of overlapping genes
+                        maxGeneCoord = [geneCoord[0]-threshDist,geneCoord[1]+threshDist]
+                        maxCheck = AuxiliaryFunctions.overlap(coord,maxGeneCoord)
+                        if(maxCheck == 0): genesList.append(assocDict[chrName][counter][2]+"_DIST") # If it overlapped then put current gene in overlap list
+                    
+                        # Verify overlap again using maximum distance with previous gene+promoter
+                        if(counter > 0): # Do this verification only if this is not the first gene
+                            if(assocDict[chrName][counter-1][4] == "+"): geneCoordPrev = [assocDict[chrName][counter-1][0]-promoterLength,assocDict[chrName][counter-1][1]]
+                            else: geneCoordPrev = [assocDict[chrName][counter-1][0],assocDict[chrName][counter-1][1]+promoterLength]
+                            maxGeneCoordPrev = [geneCoordPrev[0]-threshDist,geneCoordPrev[1]+threshDist]
+                            maxCheckPrev = AuxiliaryFunctions.overlap(coord,maxGeneCoordPrev)
+                            if(maxCheckPrev == 0): genesList.append(assocDict[chrName][counter-1][2]+"_DIST") # If it overlapped then put previous gene in overlap list
+
+                        # Verify if genes are enriched
+                        if(len(genesList) == 0): genesList.append(".") # If genesList is empty then put a '.' to represent non-association
+                        else: # If genesList is not empty then verify enriched genes
+                            for i in range(0,len(genesList)): # Check if genes in genesList are enriched 
+                                if(not geneFlagDict[genesList[i][:-5]]): genesList[i] = "."+genesList[i] # If the gene is not enriched, put a dot (.) in front of it (will be used by motif matching)
+                                #else: If gene is enriched than let the name without the dot (.) in front of it
+                        didBreak = True
+                        break
+
+                    #elif(check == -1): If gene is before coordinate, dont do anything! Just move to the next gene.
+                    counter += 1
+
+                # If not breaked, then the gene list is over and the coordinate can only overlap the last gene.
+                if(not didBreak): 
+                    genesList = []
+                    if(len(assocDict[chrName]) == 0): # If gene list is empty then dont associate coordinate with any gene
+                        genesList.append(".")
+                    else: # If gene list is not empty then try to verify overlap between coordinate and last gene
+                        if(assocDict[chrName][counter-1][4] == "+"): geneCoordPrev = [assocDict[chrName][counter-1][0]-promoterLength,assocDict[chrName][counter-1][1]]
+                        else: geneCoordPrev = [assocDict[chrName][counter-1][0],assocDict[chrName][counter-1][1]+promoterLength]
+                        maxGeneCoordPrev = [geneCoordPrev[0]-threshDist,geneCoordPrev[1]+threshDist]
+                        maxCheckPrev = AuxiliaryFunctions.overlap(coord,maxGeneCoordPrev)
+                        if(maxCheckPrev == 0): # If it overlapped then put previous gene in overlap list and check for enrichment
+                            genesList.append(assocDict[chrName][counter-1][2]+"_DIST")
+                            if(not geneFlagDict[genesList[0][:-5]]): genesList[0] = "."+genesList[0]
+                        else: genesList.append(".") # If does not overlap then put a '.' to represent non-association
+
+                # Write the curent coordinate with its corresponding overlapping genes (enriched or not)
+                aDict[chrName].append(coord[:5]) # Writing the raw coordinate until STRAND field only
+                aDict[chrName][-1][2] = ":".join(genesList) # Write list of overlapping genes
+                aDict[chrName][-1][3] = 0 # Force the SCORE field to be '0'
+                aDict[chrName][-1][4] = "." # Force the STRAND field to be '.'
+
+        # Converting aDict to genomic region set
+        result_grs = GenomicRegionSet(self.name+"_associated")
+        for chrom in aDict.keys():
+            for coord in aDict[chrom]:
+                curr_genes_list = coord[2].split(":")
+                new_genes_list = []
+                new_prox_list = []
+                for curr_gene in curr_genes_list:
+                    new_genes_list.append("_".join(curr_gene.split("_")[:-1]))
+                    new_prox_list.append(curr_gene.split("_")[-1])
+                new_genes_list = ":".join(new_genes_list)
+                new_prox_list = ":".join(new_prox_list)
+                result_grs.add(GenomicRegion(chrom, coord[0], coord[1], name=new_genes_list, data=new_prox_list))
+             
+            return result_grs
+
+    def filter_by_gene_association(self, gene_set = None, organism = "hg19", promoterLength = 1000, threshDist = 50000):
+        """Updates self in order to keep only the coordinates associated to genes which are in gene_set.
+           It also returns information regarding the mapped genes and proximity information (if mapped in proximal (PROX)
+           or distal (DIST) regions). If a region was associated with two genes, one in the gene_set and the other not in
+           gene_set, then the updated self still keeps information about both genes. However, the latter won't be reported
+           as mapped gene.
 
            Keyword arguments:
-             fileName -- TODO
-             geneSet -- TODO
-             geneAnnotation -- TODO
-             genomeSize -- TODO
+             gene_set -- List of gene names as a GeneSet object. If None, then consider all genes to be enriched. (default None)
+             organism -- Organism in order to fetch genomic data. (default hg19)
              promoterLength -- Length of the promoter region. (default 1000)
              threshDist -- Threshold maximum distance for a coordinate to be considered associated with a gene. (default 50000)
 
            Returns:
-             le -- TODO
-             len(self.genes) -- TODO
-             mappedGenes -- TODO
-             totalPeaks -- TODO
-             regionsToGenes -- TODO
-       """
-        # TODO: Eduardo should check this!
+             None -- Updates self in order to keep only the coordinates associated to genes which are in gene_set
+             all_genes = GeneSet that contains all genes associated with the coordinates
+             mapped_genes = GeneSet that contains the genes associated with the coordinates which are in gene_set
+             all_proxs = List that contains all the proximity information of genes associated with the coordinates
+             mapped_proxs = List that contains all the proximity information of genes associated with the coordinates which are in gene_set
+        """
+
+        # Making association with gene_set
+        assoc_grs = self.gene_association(gene_set, organism, promoterLength, threshDist)
+
+        # Initializing updated genomic region set
+        updated_grs = GenomicRegionSet(assoc_grs.name) # New GRS will contain only coordinates associated to genes which are in gene_set
+
+        # Initializing resulting gene sets
+        all_genes = GeneSet("all_genes") # Contains all genes associated with the coordinates
+        mapped_genes = GeneSet("mapped_genes") # Contains the genes associated with the coordinates which are in gene_set
+        all_proxs = [] # Contains all the proximity information of genes associated with the coordinates
+        mapped_proxs = [] # Contains all the proximity information of genes associated with the coordinates which are in gene_set
+
+        # Iterating on associated genomic regions
+        for gr in assoc_grs.sequences:
+
+            # If the coordinate wasn't associated with any gene, it is not considered
+            if(gr.name == "."): continue
+
+            # Fetching list of genes and proximity information
+            curr_genes = gr.name.split(":")
+            curr_proxs = gr.data.split(":")
+
+            # Updating all/mapped - genes/proxs
+            flag_assoc = False
+            for i in range(0,len(curr_genes)):
+                g = curr_genes[i]
+                p = curr_proxs[i]
+                if(g[0] != "."):
+                    mapped_genes.genes.append(g)
+                    mapped_proxs.append(p)
+                    flag_assoc = True
+                else: g = g[1:]
+                all_genes.genes.append(g)
+                all_proxs.append(p)
+            
+            # Adding current genomic region to updated GRS if there was a gene associated with it in gene_set
+            if(flag_assoc): updated_grs.add(deepcopy(gr))
+
+        # Removing duplicates
+        all_genes.genes = list(set(all_genes.genes))
+        mapped_genes.genes = list(set(mapped_genes.genes))
+        all_proxs = list(set(all_proxs))
+        mapped_proxs = list(set(mapped_proxs))
+
+        # Updating self
+        self.name = updated_grs.name
+        self.sequences = updated_grs.sequences
+        self.sorted = False
+
+        return all_genes, mapped_genes, all_proxs, mapped_proxs
+
+    """
+    def filter_by_gene_association(self,fileName,geneSet,geneAnnotation,genomeSize,promoterLength=1000,threshDist=50000):
         from rgt.motifanalysis.util import bedFunctions, sort
         from rgt.motifanalysis.enrichment.geneAssociation import *
         self.fileName=fileName
@@ -175,7 +388,9 @@ class GenomicRegionSet:
           le=0
         else:
           le=len(geneSet)
+
         return le, len(self.genes), mappedGenes, totalPeaks,regionsToGenes
+    """
 
     def intersect(self,y,mode=OverlapType.OVERLAP):
         """Return the overlapping regions with three different modes.
@@ -359,7 +574,7 @@ class GenomicRegionSet:
         if len(self) == 0 or len(y) == 0:
             return GenomicRegionSet('None region')
         # Establish an extended GenomicRegionSet
-        extended_self = copy.deepcopy(self)
+        extended_self = deepcopy(self)
         for i in extended_self:
             i.extend(adding_length,adding_length)
         # Find their intersections
@@ -517,7 +732,7 @@ class GenomicRegionSet:
             z = GenomicRegionSet('Clustered region set')
             previous = self.sequences[0]
             for s in self.sequences[1:]:
-                s_ext = copy.deepcopy(s)
+                s_ext = deepcopy(s)
                 s_ext.extend(max_distance,max_distance)
                 if s_ext.overlap(previous):
                     previous.initial = min(previous.initial, s.initial)
