9a9468df17efc70d40610133823cef76a3ebac08
jovesus@gmail.com <jovesus@gmail.com@7ac398c0-e0aa-3817-0650-6b828aca5894>
2015-01-02 09:36:01 +0000

    M    rgt/SequenceSet.py M    rgt/triplex/Main.py M    rgt/triplex/triplexTools.py

--- trunk/rgt/SequenceSet.py
+++ trunk/rgt/SequenceSet.py
@@ -4,17 +4,52 @@ import os
 # Local Libraries
 
 # Distal Libraries
-from Sequence import Sequence
 from Util import SequenceType
 from rgt.GenomicRegionSet import GenomicRegionSet
 
+####################################################################################
+####################################################################################
 
-"""
-Define the collection of sequences and their functions
-Author: joseph Kuo
-"""
+class Sequence():
+    """Describe the Sequence with ATCG as alphabets as well as its types
 
-class SequenceSet:
+    Author: JosephKuo
+    """
+
+    def __init__(self, seq, seq_type=SequenceType.DNA, name="",  strand="+"):
+        """Initiation"""
+        self.seq_type = seq_type # DNA or RNA
+        self.name = name # Extra information about the sequence
+        self.seq = seq.upper() # Convert all alphabets into upper case
+        self.strand = strand
+        
+    def __len__(self):
+        """Return the length of the sequence"""
+        return len(self.seq)
+        
+    def dna_to_rna(self):
+        """Convert the sequence from DNA sequence to RNA sequence"""
+        self.seq = self.seq.replace("T","U")
+        self.seq_type = SequenceType.DNA
+        
+    def rna_to_dna(self):
+        """Convert the sequence from RNA sequence to DNA sequence"""
+        self.seq = self.seq.replace("U", "T")
+        self.seq_type = SequenceType.RNA
+        
+    def GC_content(self):
+        """Return the ratio of GC content in this sequence"""
+        gc = self.seq.count("G") + self.seq.count("C")
+        return gc/float(len(self))
+
+####################################################################################
+####################################################################################
+ 
+class SequenceSet:           
+    """Define the collection of sequences and their functions
+    Author: joseph Kuo
+    """
+
     def __init__(self, name, seq_type):
         """Initiation"""
         self.name = name
@@ -48,21 +83,24 @@ class SequenceSet:
             except: # Start to parse a new sequence
                 pass
 
+    
 
     def read_bed(self, bedfile, genome_file_dir):
         """Read the sequences defined by BED file on the given genomce"""
         # Read BED into GenomicRegionSet
-        bed = GenomicRegionSet(os.path.basename(bedfile))
-        bed.read_bed(bedfile)
+    #    bed = GenomicRegionSet(os.path.basename(bedfile))
+    #    bed.read_bed(bedfile)
         
         # Parse each chromosome and fetch the defined region in this chromosome
-        for s in bed:
+    #    for s in bed:
             
         
-        
+    pass
         
         # Read genome by chromosome
         
+####################################################################################
+####################################################################################
 
 if __name__ == '__main__':
     a = SequenceSet(name="test", seq_type=SequenceType.DNA)
