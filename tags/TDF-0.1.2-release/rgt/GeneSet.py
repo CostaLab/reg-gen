ec7e88630f854401eea70c593cd480e57ff0de48
jovesus@gmail.com <jovesus@gmail.com@7ac398c0-e0aa-3817-0650-6b828aca5894>
2015-03-27 13:27:55 +0000

    make tag for TDF-0.1.2

---
+++ tags/TDF-0.1.2-release/rgt/GeneSet.py
@@ -0,0 +1,70 @@
+from Util import GenomeData
+"""
+Describe genes and their expression.
+
+Authors: Ivan G. Costa, Manuel Allhoff
+
+"""
+
+class GeneSet:
+    
+    def __init__(self,name):
+        self.name = name
+        self.genes = [] #list of genes to consider
+        self.values = {} #keys: gene, value: expression data as a list
+        self.cond = []
+
+    def __len__(self):
+        return len(self.genes)
+    
+    def __iter__(self):
+        return iter(self.genes)
+
+    def read(self, geneListFile, header=False, extra_column=False):
+        """Read genes"""
+        with open(geneListFile) as f:
+            if extra_column:
+                self.extra_column = {}
+                for line in f:            
+                    line = line.strip()
+                    l = line.split()
+                    
+                    if len(l) > 1:
+                        self.genes.append(l[0])
+                        self.extra_column[l[0]] = l[1]
+            else:
+                lines = (line.rstrip() for line in f)
+                self.genes = list(set((line for line in lines if line))) # Non-blank lines in a list
+            
+    def read_expression(self, geneListFile, header = True):
+        """Read gene expression data"""
+        with open(geneListFile) as f:
+            if header:
+                l = f.readline()
+                l = l.strip("\n")
+                l = l.split("\t")
+                self.cond = l[1:len(l)]
+            for l in f.readlines():
+                l = l.strip("\n")
+                l = l.split("\t")
+                self.genes.append(l[0].upper())
+                #self.values[l[0].upper()] = [float(v) for v in l[1:len(l)]]
+                self.values[l[0].upper()] = float(l[1])
+
+    def get_all_genes(self, organism):
+        """Get all gene names for a given organism"""
+        genome = GenomeData(organism=organism)
+        self.genes = []
+        f = open(genome.get_association_file())
+        for l in f.readlines():
+            l = l.strip("\n")
+            l = l.split("\t")
+            self.genes.append(l[3].upper())
+        f.close()
+
+        self.genes = list(set(self.genes))
+
+    def subtract(self, gene_set):
+        """Subtract another GeneSet"""
+        self.genes = [gene for gene in self.genes if gene not in gene_set.genes]
+
