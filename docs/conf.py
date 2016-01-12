c70d389ff27ae2f9f3879c9f526b552a26afc79a
Manuel Allhoff <allhoff@aices.rwth-aachen.de>
2016-01-12 11:42:01 +0100

    new

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
