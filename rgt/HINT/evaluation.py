9ed2a38ae286572051d5911f77d9e8d54dee3dda
Zhijian Li <lzj1769@gmail.com>
2017-01-18 13:28:22 +0100

    modified evaluation.py

--- rgt/HINT/evaluation.py
+++ rgt/HINT/evaluation.py
@@ -7,10 +7,8 @@ from __future__ import print_function
 import numpy as np
 import pylab
 from sklearn import metrics
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-
+plt.switch_backend('agg')
 # Internal
 from rgt.GenomicRegionSet import GenomicRegionSet
 from rgt.Util import OverlapType
