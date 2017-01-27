bc85395450f7e98445813a1136b4c34c32781059
Zhijian Li <lzj1769@gmail.com>
2017-01-27 10:56:37 +0100

    modified bias table options

--- rgt/HINT/train.py
+++ rgt/HINT/train.py
@@ -66,7 +66,7 @@ class TrainHMM:
             if self.original_regions.split(".")[-1] == "fa":
                 regions.read_sequence(self.original_regions)
 
-            bias_table = BiasTable(regions=regions, dnase_file_name=self.bam_file,
+            bias_table = BiasTable().estimate_table(regions=regions, dnase_file_name=self.bam_file,
                                     genome_file_name=genome_data.get_genome(), k_nb=self.k_nb, shift=self.shift)
 
             bias_fname = os.path.join(self.output_locaiton, "Bias", "{}_{}".format(self.k_nb, self.shift))
