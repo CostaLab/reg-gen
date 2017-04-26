2627169a8e4996ac22cca88b006d304f4606842a
jovesus <jovesus@gmail.com>
2017-04-26 12:15:10 +0200

    modified:   tools/rgt-tools.py

--- tools/rgt-tools.py
+++ tools/rgt-tools.py
@@ -1153,8 +1153,8 @@ if __name__ == "__main__":
             with open(args.o, "w") as fout:
                 for line in fin:
                     if line.startswith("chr"):
-                        line = line.strip()
-                        l = line.split()
+
+                        l = line.strip().split()
                         overlapping = overlap_regions.covered_by_aregion(GenomicRegion(chrom=l[0], initial=int(l[1]), final=int(l[2])))
                         if len(overlapping) > 0:
                             print(line + "\t" + overlapping.sequences[0].name, file=fout)
@@ -1332,7 +1332,7 @@ if __name__ == "__main__":
 
         for region in bed2:
             data = region.data.split()
-            stat = data[4].split(";")
+            stat = data[5].split(";")
             s1 = [float(x) + 1 for x in stat[0].split(":")]
             s2 = [float(x) + 1 for x in stat[1].split(":")]
             fc = math.log((sum(s2) / len(s2)) / (sum(s1) / len(s1)), 2)
@@ -1345,7 +1345,7 @@ if __name__ == "__main__":
 
         for region in bed2:
             l = region.data.split()
-            s = l[4].split(";")
+            s = l[5].split(";")
             if abs(float(l[0])) > args.fc and float(s[2]) > args.p:
                 s1 = sum([int(x) for x in s[0].split(":")]) / len(s[0].split(":"))
                 s2 = sum([int(x) for x in s[1].split(":")]) / len(s[1].split(":"))
