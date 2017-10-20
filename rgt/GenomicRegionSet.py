03a0dcff04c5067f44f5fbdbb3702a7dba7db56b
jovesus <jovesus@gmail.com>
2017-10-20 11:12:32 +0200

    modified:   rgt/GenomicRegionSet.py

--- rgt/GenomicRegionSet.py
+++ rgt/GenomicRegionSet.py
@@ -416,7 +416,7 @@ class GenomicRegionSet:
         return a, b
     
     def gene_association(self, organism, gene_set=None, promoterLength=1000,
-                        threshDist=100000, show_dis=False, strand_specific=False):
+                         threshDist=100000, show_dis=False, strand_specific=False):
         """Associates coordinates to genes given the following rules:
 
             1. If the peak is inside gene (promoter+coding) then this peak is associated with that gene.
@@ -616,7 +616,7 @@ class GenomicRegionSet:
         """
 
         # Making association with gene_set
-        assoc_grs = self.gene_association(gene_set, organism, promoterLength, threshDist)
+        assoc_grs = self.gene_association(organism=organism, gene_set=gene_set, promoterLength=promoterLength, threshDist=threshDist)
 
         # Initializing updated genomic region set
         updated_grs = GenomicRegionSet(assoc_grs.name) # New GRS will contain only coordinates associated to genes which are in gene_set
