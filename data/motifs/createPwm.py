96b5cb83cb246e4a9635b04c1263882b14bc42c9
lzj1769 <lzj1769@gmail.com>
2017-12-15 18:05:22 +0100

    format pwm files

--- data/motifs/createPwm.py
+++ data/motifs/createPwm.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 inputFileName = sys.argv[1]
 outputFolder = sys.argv[2]
@@ -18,6 +19,10 @@ if inputFormat == "jaspar-2014":
         count_c = content[i * 5 + 2].strip()
         count_g = content[i * 5 + 3].strip()
         count_t = content[i * 5 + 4].strip()
+        count_a = re.sub('\s+', ' ', count_a)
+        count_c = re.sub('\s+', ' ', count_c)
+        count_g = re.sub('\s+', ' ', count_g)
+        count_t = re.sub('\s+', ' ', count_t)
 
         outputFileName = os.path.join(outputFolder, "{}.pwm".format(motif_name.replace(">", "")))
         with open(outputFileName, "w") as f:
@@ -29,12 +34,16 @@ if inputFormat == "jaspar-2014":
 elif inputFormat == "jaspar-2016":
     for i in range(n_lines/5):
         motif_name = content[i * 5 + 0].replace(">","").replace("\t", ".").strip()
-        count_a = content[i * 5 + 1].replace("A","").replace("[","").replace("]","").strip()
-        count_c = content[i * 5 + 2].replace("C","").replace("[","").replace("]","").strip()
-        count_g = content[i * 5 + 3].replace("G","").replace("[","").replace("]","").strip()
-        count_t = content[i * 5 + 4].replace("T","").replace("[","").replace("]","").strip()
+        count_a = content[i * 5 + 1].translate(None, '[A]').strip()
+        count_c = content[i * 5 + 2].translate(None, '[C]').strip()
+        count_g = content[i * 5 + 3].translate(None, '[G]').strip()
+        count_t = content[i * 5 + 4].translate(None, '[T]').strip()
+        count_a = re.sub('\s+', ' ', count_a)
+        count_c = re.sub('\s+', ' ', count_c)
+        count_g = re.sub('\s+', ' ', count_g)
+        count_t = re.sub('\s+', ' ', count_t)
 
-        outputFileName = os.path.join(outputFolder, "{}.pwm".format(motif_name.replace(">", "")))
+        outputFileName = os.path.join(outputFolder, "{}.pwm".format(motif_name))
         with open(outputFileName, "w") as f:
             f.write(count_a + "\n")
             f.write(count_c + "\n")
