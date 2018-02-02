ce807f1b0b0a28537a3a8735bd6d2e9c1b619d8a
Zhijian Li <lzj1769@gmail.com>
2018-02-02 19:15:38 +0100

    commit

--- rgt/HINT/DifferentialAnalysis.py
+++ rgt/HINT/DifferentialAnalysis.py
@@ -9,7 +9,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 import pyx
-from scipy import stats
+from scipy.stats.mvn import mvnun
 from argparse import SUPPRESS
 
 # Internal
@@ -57,8 +57,8 @@ def diff_analysis_args(parser):
                         help="The name of condition1. DEFAULT: condition1")
     parser.add_argument("--condition2", type=str, metavar="STRING", default="condition1",
                         help="The name of condition2. DEFAULT: condition2")
-    parser.add_argument("--fdr", type=float, metavar="FLOAT", default=0.001,
-                        help="The false discovery rate. DEFAULT: 0.001")
+    parser.add_argument("--fdr", type=float, metavar="FLOAT", default=0.05,
+                        help="The false discovery rate. DEFAULT: 0.05")
 
     # Output Options
     parser.add_argument("--output-location", type=str, metavar="PATH", default=os.getcwd(),
@@ -213,12 +213,11 @@ def diff_analysis_run(args):
         ps_tc_results_by_tf[mpbs_name] = get_ps_tc_results(args, signal_dict_by_tf_1[mpbs_name],
                                                            signal_dict_by_tf_2[mpbs_name], motif_len_dict[mpbs_name])
 
-    output_results(args, ps_tc_results_by_tf)
+    #output_results(args, ps_tc_results_by_tf)
 
-
-#    stat_results_by_tf = get_stat_results(ps_tc_results_by_tf)
-#    scatter_plot(args, stat_results_by_tf)
-#    output_stat_results(args, stat_results_by_tf)
+    stat_results_by_tf = get_stat_results(ps_tc_results_by_tf)
+    scatter_plot(args, stat_results_by_tf)
+    output_stat_results(args, stat_results_by_tf)
 
 
 def get_bc_signal(chrom, start, end, bam, bias_table, genome_file_name, forward_shift, reverse_shift):
@@ -240,7 +239,7 @@ def get_bc_signal(chrom, start, end, bam, bias_table, genome_file_name, forward_
     if (p1 <= 0 or p1_w <= 0 or p2_wk <= 0):
         # Return raw counts
         bc_signal = [0.0] * (p2 - p1)
-        for read in self.bam.fetch(chrName, p1, p2):
+        for read in bam.fetch(chrom, p1, p2):
             if not read.is_reverse:
                 cut_site = read.pos + forward_shift
                 if p1 <= cut_site < p2:
@@ -337,6 +336,9 @@ def get_ps_tc_results(args, signal_list_1, signal_list_2, motif_len):
     signal_1 = (signal_1 / args.factor1) / num_fp
     signal_2 = (signal_2 / args.factor2) / num_fp
 
+    signal_1 = standard(signal_1)
+    signal_2 = standard(signal_2)
+
     signal_half_len = len(signal_1) / 2
 
     nc = sum(signal_1[signal_half_len - motif_len / 2:signal_half_len + motif_len / 2])
@@ -413,6 +415,7 @@ def compute_factors(signal_dict_by_tf_1, signal_dict_by_tf_2):
 
 def line_plot(args, mpbs_name, num_fp, signal_tf_1, signal_tf_2, pwm_dict, fig, ax):
     # compute the average signal
+    mpbs_filename = mpbs_name.replace("(", ".").replace("(", "")
     mean_signal_1 = np.zeros(args.window_size)
     mean_signal_2 = np.zeros(args.window_size)
     for i in range(num_fp):
@@ -422,20 +425,20 @@ def line_plot(args, mpbs_name, num_fp, signal_tf_1, signal_tf_2, pwm_dict, fig, 
     mean_signal_1 = (mean_signal_1 / num_fp) / args.factor1
     mean_signal_2 = (mean_signal_2 / num_fp) / args.factor2
 
-    if args.standardize:
-        mean_signal_1 = standard(mean_signal_1)
-        mean_signal_2 = standard(mean_signal_2)
+    #if args.standardize:
+    mean_signal_1 = standard(mean_signal_1)
+    mean_signal_2 = standard(mean_signal_2)
 
     # Output PWM and create logo
     pwm_fname = os.path.join(args.output_location, "{}_{}".format(args.condition1, args.condition2),
-                             "{}.pwm".format(mpbs_name))
+                             "{}.pwm".format(mpbs_filename))
     pwm_file = open(pwm_fname, "w")
     for e in ["A", "C", "G", "T"]:
         pwm_file.write(" ".join([str(int(f)) for f in pwm_dict[e]]) + "\n")
     pwm_file.close()
 
     logo_fname = os.path.join(args.output_location, "{}_{}".format(args.condition1, args.condition2),
-                              "{}.logo.eps".format(mpbs_name))
+                              "{}.logo.eps".format(mpbs_filename))
     pwm = motifs.read(open(pwm_fname), "pfm")
     pwm.weblogo(logo_fname, format="eps", stack_width="large", stacks_per_line=str(args.window_size),
                 color_scheme="color_classic", unit_name="", show_errorbars=False, logo_title="",
@@ -471,39 +474,43 @@ def line_plot(args, mpbs_name, num_fp, signal_tf_1, signal_tf_2, pwm_dict, fig, 
     ax.spines['bottom'].set_position(('outward', 70))
 
     figure_name = os.path.join(args.output_location, "{}_{}".format(args.condition1, args.condition2),
-                               "{}.line.eps".format(mpbs_name))
+                               "{}.line.eps".format(mpbs_filename))
     fig.tight_layout()
     fig.savefig(figure_name, format="eps", dpi=300)
 
     # Creating canvas and printing eps / pdf with merged results
     output_fname = os.path.join(args.output_location, "{}_{}".format(args.condition1, args.condition2),
-                                "{}.eps".format(mpbs_name))
+                                "{}.eps".format(mpbs_filename))
+
     c = pyx.canvas.canvas()
     c.insert(pyx.epsfile.epsfile(0, 0, figure_name, scale=1.0))
     c.insert(pyx.epsfile.epsfile(0.45, 0.8, logo_fname, width=16.5, height=3))
     c.writeEPSfile(output_fname)
-    os.system("epstopdf " + output_fname)
+    os.system(" ".join(["epstopdf", output_fname]))
 
     os.remove(figure_name)
     os.remove(logo_fname)
-    os.remove(output_fname)
+    #os.remove(output_fname)
     os.remove(pwm_fname)
 
 
 def scatter_plot(args, stat_results_by_tf):
     tc_diff = list()
     ps_diff = list()
-    mpbs_names = list()
-    for mpbs_name in stat_results_by_tf.keys():
-        mpbs_names.append(mpbs_name)
-        tc_diff.append(stat_results_by_tf[mpbs_name][-2])
-        ps_diff.append(stat_results_by_tf[mpbs_name][2])
+    mpbs_name_list = stat_results_by_tf.keys()
+    P_values = list()
+    for mpbs_name in mpbs_name_list:
+        ps_diff.append(float(stat_results_by_tf[mpbs_name][2]))
+        tc_diff.append(float(stat_results_by_tf[mpbs_name][-3]))
+        P_values.append(np.log10(float(stat_results_by_tf[mpbs_name][-1])))
 
     fig, ax = plt.subplots(figsize=(12, 12))
-    ax.scatter(tc_diff, ps_diff, alpha=0.0)
-    for i, mpbs_name in enumerate(mpbs_names):
+    for i, mpbs_name in enumerate(mpbs_name_list):
         if stat_results_by_tf[mpbs_name][-1] < args.fdr:
+            ax.scatter(tc_diff[i], ps_diff[i], c="red")
             ax.annotate(mpbs_name, (tc_diff[i], ps_diff[i]), alpha=0.6)
+        else:
+            ax.scatter(tc_diff[i], ps_diff[i], c="black", alpha=0.6)
     ax.margins(0.05)
 
     tc_diff_mean = np.mean(tc_diff)
@@ -521,7 +528,7 @@ def scatter_plot(args, stat_results_by_tf):
 
 def output_results(args, ps_tc_results_by_tf):
     mpbs_name_list = ps_tc_results_by_tf.keys()
-    header = ["Motif", "Num",
+    header = ["Motif",
               "Protection_Score_{}".format(args.condition1), "Protection_Score_{}".format(args.condition2),
               "Protection_Diff_{}_{}".format(args.condition1, args.condition2),
               "TC_{}".format(args.condition1), "TC_{}".format(args.condition2),
@@ -540,7 +547,6 @@ def output_stat_results(args, stat_results_by_tf):
               "Protection_Diff_{}_{}".format(args.condition1, args.condition2),
               "TC_{}".format(args.condition1), "TC_{}".format(args.condition2),
               "TC_Diff_{}_{}".format(args.condition1, args.condition2), "P_values", "Adjust_p_values"]
-
     with open(output_fname, "w") as f:
         f.write("\t".join(header) + "\n")
         for mpbs_name in stat_results_by_tf.keys():
@@ -564,54 +570,44 @@ def output_mu(args, median_diff_prot, median_diff_tc):
 
 
 def get_stat_results(ps_tc_results_by_tf):
-    stat_results_by_tf = dict()
-    pop_ps_diff = list()
-    pop_tc_diff = list()
-    for mpbs_name in ps_tc_results_by_tf.keys():
-        stat_results_by_tf[mpbs_name] = list()
-        mean_results = np.zeros(6)
-        num = 0
-        for i in range(len(ps_tc_results_by_tf[mpbs_name])):
-            mean_results = np.add(mean_results, np.array(ps_tc_results_by_tf[mpbs_name][i]))
-            pop_ps_diff.append(ps_tc_results_by_tf[mpbs_name][i][2])
-            pop_tc_diff.append(ps_tc_results_by_tf[mpbs_name][i][-1])
-            num += 1
-        mean_results = mean_results / num
-        stat_results_by_tf[mpbs_name] = mean_results.tolist()
-
-    # Two-sample Hotelling's T-Square test population and specific tf
-    population = np.array([pop_ps_diff, pop_tc_diff]).T
-    cov_pop = np.cov(population.T)
-    n1 = population.shape[0]
-    mean1 = population.mean(axis=0)
-
-    for mpbs_name in ps_tc_results_by_tf.keys():
-        ps_diff_by_tf = list()
-        tc_diff_by_tf = list()
-        for i in range(len(ps_tc_results_by_tf[mpbs_name])):
-            ps_diff_by_tf.append(ps_tc_results_by_tf[mpbs_name][i][2])
-            tc_diff_by_tf.append(ps_tc_results_by_tf[mpbs_name][i][-1])
-
-        tf = np.array([ps_diff_by_tf, tc_diff_by_tf]).T
-        cov_tf = np.cov(tf.T)
-        n2 = tf.shape[0]
-        mean2 = tf.mean(axis=0)
-
-        k = tf.shape[1]
-        t2 = np.dot(np.dot((mean1 - mean2).T, np.linalg.inv(cov_pop / n1 + cov_tf / n2)), (mean1 - mean2))
-        p_value = stats.chi2.sf(t2, k)
-        stat_results_by_tf[mpbs_name].append(p_value)
-
-    p_values = list()
-    mpbs_name_list = stat_results_by_tf.keys()
+    ps_diff = list()
+    tc_diff = list()
+    mpbs_name_list = ps_tc_results_by_tf.keys()
     for mpbs_name in mpbs_name_list:
-        p_values.append(stat_results_by_tf[mpbs_name][-1])
+        ps_diff.append(ps_tc_results_by_tf[mpbs_name][2])
+        tc_diff.append(ps_tc_results_by_tf[mpbs_name][-1])
+
+    ps_tc_diff = np.array([ps_diff, tc_diff]).T
+    mu = np.mean(ps_tc_diff, axis=0)
+    cov_ps_tc_diff = np.cov(ps_tc_diff.T)
+
+    low = np.zeros(2)
+    upp = np.zeros(2)
+    p_values = list()
+    for idx, mpbs_name in enumerate(mpbs_name_list):
+        if ps_diff[idx] >= mu[0]:
+            low[0] = ps_diff[idx]
+            upp[0] = float('inf')
+        else:
+            low[0] = -float('inf')
+            upp[0] = ps_diff[idx]
+
+        if tc_diff[idx] >= mu[1]:
+            low[1] = tc_diff[idx]
+            upp[1] = float('inf')
+        else:
+            low[1] = -float('inf')
+            upp[1] = tc_diff[idx]
+
+        p_value, i = mvnun(low, upp, mu, cov_ps_tc_diff)
+        ps_tc_results_by_tf[mpbs_name].append(p_value)
+        p_values.append(p_value)
 
     adjusted_p_values = adjust_p_values(p_values)
     for idx, mpbs_name in enumerate(mpbs_name_list):
-        stat_results_by_tf[mpbs_name].append(adjusted_p_values[idx])
+        ps_tc_results_by_tf[mpbs_name].append(adjusted_p_values[idx])
 
-    return stat_results_by_tf
+    return ps_tc_results_by_tf
 
 
 def standard(vector):
