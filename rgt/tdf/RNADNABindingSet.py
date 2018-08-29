5b44d8f7ed5614d6f37ce397b01b0dd1baf2a55c
ronghui1992 <hilironghui@gmail.com>
2018-08-29 16:06:31 +0200

    modified combine motif

--- rgt/tdf/RNADNABindingSet.py
+++ rgt/tdf/RNADNABindingSet.py
@@ -58,8 +58,8 @@ class RNADNABinding:
     
     def __str__(self):
         return "\t".join( [ self.rna.chrom, str(self.rna.initial), str(self.rna.final), self.dna.toString(), "0", 
-                            str(len(self.dna)), self.score, self.err_rate, self.err, self.motif,
-                            self.strand, self.orient, self.guan_rate ] )
+                            str(len(self.dna)), str(self.score), str(self.err_rate), str(self.err), str(self.motif),
+                            str(self.strand), str(self.orient), str(self.guan_rate) ] )
 
     def __eq__(self, other):
         return (self.dna, self.rna) == (other.dna, other.rna)
