e94f78cbc92d216c2c5530021ad9c80a2752bece
ronghui1992 <hilironghui@gmail.com>
2018-09-05 15:46:57 +0200

    dna rna gap fix

--- rgt/tdf/RNADNABindingSet.py
+++ rgt/tdf/RNADNABindingSet.py
@@ -1004,9 +1004,9 @@ class RNADNABindingSet:
                 # different RNA binding motif and same parallel
                 if cur_rbs.rna.motif != next_rbs.rna.motif or cur_rbs.rna.orientation != next_rbs.rna.orientation:
                     # no RNA overlap
-                    if cur_rbs.rna.distance(next_rbs.rna) > 0 and cur_rbs.dna.distance(next_rbs.dna):
+                    if cur_rbs.rna.distance(next_rbs.rna) > 0 and cur_rbs.dna.distance(next_rbs.dna) > 0:
                         # DNA gap smaller than 10bp and no overlap
-                        if 0 < int(cur_rbs.dna.distance(next_rbs.dna)) <= int(cur_rbs.rna.distance(next_rbs.rna)): #5) and (cur_rbs.dna.chrom == next_rbs.dna.chrom):
+                        if 0 < self.region_distance(cur_rbs.dna, next_rbs.dna) <= self.region_distance(cur_rbs.rna, next_rbs.rna): #5) and (cur_rbs.dna.chrom == next_rbs.dna.chrom):
                             # same strand on the DNA
                             if cur_rbs.dna.orientation == next_rbs.dna.orientation:
                                 # combine motif - Parallel
